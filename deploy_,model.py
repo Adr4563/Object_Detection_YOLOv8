68cccfaff6558e0cd21ed505b4aa43d566fa0bcd
unknown <afloresu.01@gmail.com>
2024-03-26 23:31:00 -0500

    Update code

--- deploy_,model.py
+++ deploy_,model.py
@@ -3,42 +3,44 @@ from ultralytics import YOLO
 import datetime
 
 try:    
-    # IP_Address = 'http://192.168.0.103:8080'
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0) # You can use IP_Address = 'http://192.168.0.103:8080'
 except:
-    print("Fail conection")
+    print("Fallo en la conexión")
 
 # Load the model with the best-weights
 model = YOLO(r'C:\Users\user\Desktop\Git-Repositories\Pothole_Detection\weights\best.pt')
 
+class_names = ['Pothole', 'Pothole2'] 
+
+def draw_boxes(image, boxes, class_names):
+    for box in boxes:
+        x1, y1, x2, y2 = box  # Coordenadas de la esquina superior izquierda (x1, y1) y la esquina inferior derecha (x2, y2)
+        color = (0, 255, 0)  
+        cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+    return image
+
 def register(quantity):
     date = datetime.datetime.now()
-    if (quantity != 0):
+    if quantity != 0:
         with open("batch_register.txt", "a") as archivo:
             archivo.write(f"{date}, {quantity}\n")
-        
-        
-while True:
-    # Initialize capture
-    ret, frame = cap.read()
-    # Resize the frame
-    resized_frame = cv2.resize(frame, (640,480))
-    
-    # Display the resulting frame
-    cv2.imshow("Batch-Detection", resized_frame)
-        
-    result = model(frame, verbose=False)
 
-    if isinstance(result, list) and result:
-        quantity = 0
-        for item in result:
-            quantity += len(item.boxes)  
-            
-        register(quantity)
+while True:
+    # Initialize the frame
+    ret, frame = cap.read()
+    # Redimensionar el frame
+    resized_frame = cv2.resize(frame, (640,480))
         
-    if(cv2.waitKey(30) == ord('q')):        
-            break
+    # Realizar la predicción de objetos
+    result = model.predict(frame, imgsz=640, save=False)
+    
+    new_frame = result[0].render()
         
+    cv2.imshow("Object detection", new_frame)
+        
+    if cv2.waitKey(1000) == ord('q'):
+        break
+
 cap.release()
 cv2.destroyAllWindows()
 
